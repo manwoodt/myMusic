3e919fc73bf4607353e51daccf9b3e26f17d13ab
manwoodtWindows <manwoodt@gmail.com>
2025-02-10 21:30:57 +0300

    init modules

--- MyMusicApp/presentation/build.gradle.kts
+++ MyMusicApp/presentation/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.avito.mymusic"
+    namespace = "com.avito.presentation"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.avito.mymusic"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -33,22 +30,13 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
-    implementation(project(":data"))
-    implementation(project(":domain"))
-    implementation(project(":presentation"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- MyMusicApp/settings.gradle.kts
+++ MyMusicApp/settings.gradle.kts
@@ -21,4 +21,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "MyMusic"
 include(":app")
- 
+include(":presentation")
+include(":data")
+include(":domain")
